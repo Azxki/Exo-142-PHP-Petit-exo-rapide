cca0d788072f6377d5a5e595aea9aca475ffc89b
azxki <cococoursan@gmail.com>
2023-01-11 16:48:36 +0100

    exo

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    $maVar = "text";
+    echo $maVar;
+
+    $myArray = ['yo', 'bonjour', 'hello', 'bjr'];
+    echo $myArray[2];
+
+    for ($i = 1; $i < 3; $i++) {
+        echo $myArray[$i];
+    }
+?>
+
+<ul></ul>
+
+<?php
+    foreach ($myArray as $item) {
+        echo $myArray;
+    }
+?>
